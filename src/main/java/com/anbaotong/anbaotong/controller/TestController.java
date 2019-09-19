a89e9920a47e2949314e656387632b71382e70ac
lijian <928229131@qq.com>
2019-09-19 11:40:53 +0800

    3

--- src/main/java/com/anbaotong/anbaotong/controller/TestController.java
+++ src/main/java/com/anbaotong/anbaotong/controller/TestController.java
@@ -3,7 +3,9 @@ package com.anbaotong.anbaotong.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,7 +18,14 @@ public class TestController {
     @RequestMapping("/hello")
     public Map test(){
         Map<String, Object> param = new HashMap<>();
-        param.put("name", "lijian");
+        List list = new ArrayList();
+        for (int i=0;i<2;i++){
+            Map map = new HashMap();
+            map.put("src","images/products/video_meeting/1.jpg");
+            map.put("name", "视频会议");
+            list.add(map);
+        }
+        param.put("producList", list);
         return param ;
     }
 }
