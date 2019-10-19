6d531204b0745a0d012161e7bafad9d8a2f9e2a9
lijian <928229131@qq.com>
2019-10-19 16:06:22 +0800

    添加分页组件

--- src/main/java/com/anbaotong/controller/BaikeController.java
+++ src/main/java/com/anbaotong/controller/BaikeController.java
@@ -4,19 +4,13 @@ import com.anbaotong.bean.BaikeBean;
 import com.anbaotong.bean.ImageBean;
 import com.anbaotong.bean.Result;
 import com.anbaotong.service.BaikeService;
+import com.anbaotong.util.FileUtil;
 import com.anbaotong.util.UuidUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -36,7 +30,7 @@ public class BaikeController {
         log.info(baikeBean.toString());
         String id = UuidUtil.createID();
         baikeBean.setId(id);
-        ImageBean imageBean = uploadImage(baikeBean.getFile());
+        ImageBean imageBean = FileUtil.uploadImage(baikeBean.getFile());
         imageBean.setForeignId(id);
         baikeService.addBaike(baikeBean,imageBean);
         return new Result("0","新增成功");
@@ -49,35 +43,6 @@ public class BaikeController {
     public BaikeBean getNewsDetail(BaikeBean baikeBean) {
         return baikeService.getBaikeDetail(baikeBean);
     }
-    @Value("${imagepath}")
-    private String imagepath;
-
-    public ImageBean uploadImage(MultipartFile file){
-        ImageBean image = new ImageBean();
-            //保存文件到本地文件，并保存路径到数据库
-            DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-            Calendar calendar = Calendar.getInstance();
-
-            String originalName = file.getOriginalFilename();
-
-            String newFileName = df.format(calendar.getTime()) + originalName;
-            log.info("文件的文件名为:" + newFileName);
-            image.setImgUrl("image/"+newFileName);
-            image.setNewImgName(newFileName);
-            image.setOriImgName(originalName);
-            File targetFile = new File(imagepath);
-            if (!targetFile.exists()) {
-                targetFile.mkdirs();
-            }
-            File dest = new File(imagepath + newFileName);
-            try {
-                file.transferTo(dest);
-                log.info(originalName+ "文件上传成功");
-            } catch (IOException e) {
-                log.error(e.toString(), e);
-            }
-        return image;
-        }
 
 
 }
--- src/main/java/com/anbaotong/controller/RuoDianController.java
+++ src/main/java/com/anbaotong/controller/RuoDianController.java
@@ -4,6 +4,7 @@ import com.anbaotong.bean.RuoDianBean;
 import com.anbaotong.bean.ImageBean;
 import com.anbaotong.bean.Result;
 import com.anbaotong.service.RuoDianService;
+import com.anbaotong.util.FileUtil;
 import com.anbaotong.util.UuidUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +38,7 @@ public class RuoDianController {
         log.info(ruoDianBean.toString());
         String id = UuidUtil.createID();
         ruoDianBean.setId(id);
-        ImageBean imageBean = uploadImage(ruoDianBean.getFile());
+        ImageBean imageBean = FileUtil.uploadImage(ruoDianBean.getFile());
         imageBean.setForeignId(id);
         ruoDianService.addRuoDian(ruoDianBean,imageBean);
         return new Result("0","新增成功");
@@ -54,35 +55,6 @@ public class RuoDianController {
     public List<Map> ruoDianType() {
         return ruoDianService.getRuoDianType();
     }
-    @Value("${imagepath}")
-    private String imagepath;
-
-    public ImageBean uploadImage(MultipartFile file){
-        ImageBean image = new ImageBean();
-            //保存文件到本地文件，并保存路径到数据库
-            DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-            Calendar calendar = Calendar.getInstance();
-
-            String originalName = file.getOriginalFilename();
-
-            String newFileName = df.format(calendar.getTime()) + originalName;
-            log.info("文件的文件名为:" + newFileName);
-            image.setImgUrl("image/"+newFileName);
-            image.setNewImgName(newFileName);
-            image.setOriImgName(originalName);
-            File targetFile = new File(imagepath);
-            if (!targetFile.exists()) {
-                targetFile.mkdirs();
-            }
-            File dest = new File(imagepath + newFileName);
-            try {
-                file.transferTo(dest);
-                log.info(originalName+ "文件上传成功");
-            } catch (IOException e) {
-                log.error(e.toString(), e);
-            }
-        return image;
-        }
 
 
 }
